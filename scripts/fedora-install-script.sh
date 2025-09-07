3124d624d7fbd439be35f9e7f4498bba3cae980e
Caleb Wolf <swagcalebwolf@gmail.com>
2025-09-07 10:24:24 -0500

    Update fedora-install-script.sh

--- scripts/fedora-install-script.sh
+++ scripts/fedora-install-script.sh
@@ -14,7 +14,7 @@ sudo dnf install -y dnf-plugins-core kernel-devel kernel-headers
 
 # Install essential tools
 echo "Installing essential tools..."
-sudo dnf install -y wget curl git vim gcc make dkms git vim vi bat exa fzf tmux zsh sed xargs nmm htop nmtui ncdu tremc cmus tty-clock mc playerctl notify-send xdotool youtube-dl 
+sudo dnf install -y wget curl git vim gcc make dkms git vim vi bat eza fzf tmux zsh sed xargs nnn htop nmtui ncdu cmus tty-clock mc playerctl notify-send xdotool youtube-dl 
 # Update after enabling RPM Fusion
 echo "Updating The System..."
 sudo dnf update -y
@@ -55,7 +55,7 @@ flatpak update -y
 
 # Install additional RPM applications
 echo "Installing additional RPM applications..."
-sudo dnf install -y openssh curl xorg-x11-font-utils fontconfig libreoffice lutris krita blender pavucontrol-qt bleachbit gparted steam
+sudo dnf install -y openssh curl xorg-x11-font-utils fontconfig libreoffice lutris krita blender pavucontrol-qt bleachbit gparted steam hyfetch
 
 # Install 1Password from their official website
 echo "Installing 1Password..."
