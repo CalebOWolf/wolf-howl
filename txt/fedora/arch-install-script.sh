aa9089ca436e671858e6fc4aea641b1de5441e81
CalebOWolf <swagcalebwolf@gmail.com>
2025-11-23 04:16:20 -0600

    Add arch-install-script.sh with official and AUR package lists

---
+++ txt/fedora/arch-install-script.sh
@@ -0,0 +1,5 @@
+# Arch Offical PAckages
+base base-devel git vim wget sudo networkmanager dhcpcd openssh net-tools linux linux-firmware mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon firefox vlc discord telegram-desktop steam gamescope gamemode lutris krita keepassxc gimp libreoffice-fresh thunderbird git kitty vim vi nano pipewire wireplumber pavucontrol qt5-wayland qt6-wayland xdg-user-dirs xorg-xwayland ttf-arimo-nerd noto-fonts bluez bluez-utils blueberry otf-font-awesome glu gtk2 libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-gst-plugins-base lib32-gst-plugins-good lib32-libpulse lib32-alsa-plugins lib32-alsa-lib libidn11 lib32-libidn11
+
+# AUR Packages
+aura-bin
