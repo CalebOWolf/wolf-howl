d38a1f3fba3a4392a60789c65d5796fac44cc802
Caleb Wolf <swagcalebwolf@gmail.com>
2026-03-22 10:50:17 -0500

    Update AUR and official package lists in install script
    Replaced 'google-chrome' with 'firstorm' and 'openrgb' in AUR packages. Added 'plasma man-pages' to official packages.
    
    More to come soon

--- txt/arch/arch-install-script.sh
+++ txt/arch/arch-install-script.sh
@@ -1,8 +1,9 @@
 # Arch Offical PAckages
-base base-devel git vim wget sudo networkmanager dhcpcd openssh net-tools linux linux-firmware mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon firefox vlc discord telegram-desktop steam gamescope gamemode lutris krita blender gimp keepassxc libreoffice-fresh thunderbird git kitty vim vi nano pipewire wireplumber pavucontrol qt5-wayland qt6-wayland xdg-user-dirs xorg-xwayland ttf-arimo-nerd noto-fonts bluez bluez-utils otf-font-awesome glu gufw firewalld libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-gst-plugins-base lib32-gst-plugins-good lib32-libpulse lib32-alsa-plugins lib32-alsa-lib gdk-pixbuf2 flatpak 
+base base-devel git vim wget sudo networkmanager dhcpcd openssh net-tools linux linux-firmware mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon firefox vlc discord telegram-desktop steam gamescope gamemode lutris krita blender gimp keepassxc libreoffice-fresh thunderbird git kitty vim vi nano pipewire wireplumber pavucontrol qt5-wayland qt6-wayland xdg-user-dirs xorg-xwayland ttf-arimo-nerd noto-fonts bluez bluez-utils otf-font-awesome glu gufw firewalld libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-gst-plugins-base lib32-gst-plugins-good lib32-libpulse lib32-alsa-plugins lib32-alsa-lib gdk-pixbuf2 flatpak plasma man-pages 
 
 # AUR Packages
-paru ttf-ms-fonts downgrade google-chrome pamac
+paru ttf-ms-fonts downgrade firstorm openrgb systemd-numlockontty 
+# Enable numLockOnTty service
 
 # FS Voice
 libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-gst-plugins-base lib32-gst-plugins-good lib32-libpulse lib32-alsa-plugins lib32-alsa-lib
