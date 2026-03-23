2a1983b8fc95ce8577afb807afdf70c7dde9c61c
Caleb Wolf <swagcalebwolf@gmail.com>
2026-03-22 19:11:56 -0500

    Update arch-install-script.sh with new packages
    Reorganize package lists and add additional packages.

--- txt/arch/arch-install-script.sh
+++ txt/arch/arch-install-script.sh
@@ -1,9 +1,14 @@
 # Arch Offical PAckages
-base base-devel git vim wget sudo networkmanager dhcpcd openssh net-tools linux linux-firmware mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon firefox vlc discord telegram-desktop steam gamescope gamemode lutris krita blender gimp keepassxc libreoffice-fresh thunderbird git kitty vim vi nano pipewire wireplumber pavucontrol qt5-wayland qt6-wayland xdg-user-dirs xorg-xwayland ttf-arimo-nerd noto-fonts bluez bluez-utils otf-font-awesome glu gufw firewalld libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-gst-plugins-base lib32-gst-plugins-good lib32-libpulse lib32-alsa-plugins lib32-alsa-lib gdk-pixbuf2 flatpak plasma man-pages 
-
+base base-devel git vim wget sudo networkmanager dhcpcd openssh net-tools linux linux-firmware mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon gamescope gamemode git vim nano pipewire wireplumber pavucontrol qt5-wayland qt6-wayland xdg-user-dirs xorg-xwayland ttf-arimo-nerd noto-fonts bluez bluez-utils otf-font-awesome glu gufw firewalld libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-gst-plugins-base lib32-gst-plugins-good lib32-libpulse lib32-alsa-plugins lib32-alsa-lib gdk-pixbuf2 
+# Other Packages
+flatpak man-pages bleachbit firefox vlc discord telegram-desktop steam lutris blender krita gimp keepassxc libreoffice-fresh thunderbird bazaar handbrake btop lact qbittorrent
+# Unknown/Other SRC
+Pipewire control tailscale rustdesk
 # AUR Packages
-paru ttf-ms-fonts downgrade firstorm openrgb systemd-numlockontty 
+paru ttf-ms-fonts downgrade firstorm openrgb systemd-numlockontty prismlauncher 
 # Enable numLockOnTty service
+# Flatpacks
+Blanket cpu-x dolphin mgba ppsspp rpcs3 pcxs2 flatseal gpu viewer (+screen rec) impression ktailctl protonplus protontricks protonup-qt resources mission center (control?) retroarch shortwaave spotify spruce weather
 
 # FS Voice
 libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-gst-plugins-base lib32-gst-plugins-good lib32-libpulse lib32-alsa-plugins lib32-alsa-lib
