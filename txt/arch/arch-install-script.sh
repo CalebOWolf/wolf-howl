fdf7a386d3c6a084189e15785219059a64aa145b
Caleb Wolf <swagcalebwolf@gmail.com>
2026-04-05 13:44:12 -0500

    Fix formatting in arch-install-script.sh

--- txt/arch/arch-install-script.sh
+++ txt/arch/arch-install-script.sh
@@ -1,5 +1,5 @@
 # Arch Offical PAckages
-base base-devel git vim wget sudo networkmanager dhcpcd openssh net-tools linux linux-firmware mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon gamescope gamemode git vim nano pipewire wireplumber pavucontrol qt5-wayland qt6-wayland xdg-user-dirs xorg-xwayland ttf-arimo-nerd noto-fonts bluez bluez-utils otf-font-awesome glu gufw firewalld libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-gst-plugins-base lib32-gst-plugins-good lib32-libpulse lib32-alsa-plugins lib32-alsa-lib gdk-pixbuf2
+base base-devel git vim wget sudo networkmanager dhcpcd openssh net-tools linux linux-firmware mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon gamescope gamemode git vim nano pipewire wireplumber pavucontrol qt5-wayland qt6-wayland xdg-user-dirs xorg-xwayland ttf-arimo-nerd noto-fonts bluez bluez-utils otf-font-awesome glu gufw firewalld libxcrypt-compat gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly lib32-libpulse lib32-alsa-plugins lib32-alsa-lib gdk-pixbuf2
 # Other Packages
 flatpak man-pages bleachbit firefox vlc discord telegram-desktop steam lutris blender krita gimp keepassxc libreoffice-fresh thunderbird bazaar handbrake btop htop lact qbittorrent
 # Unknown/Other SRC
