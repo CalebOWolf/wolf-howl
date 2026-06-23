558cb1c7b0ccadfd66891a9fc1dd660c6565bab6
Caleb Mignano <swagcalebwolf@gmail.com>
2026-06-22 20:14:41 -0500

    chore: migrate to individual nerd-fonts packages to replace deprecated nerdfonts set

--- txt/nixos/config/fonts.nix
+++ txt/nixos/config/fonts.nix
@@ -10,6 +10,16 @@
     noto-fonts-cjk-sans
     noto-fonts-emoji
     fira-code
-    nerdfonts
+
+    # pkgs.nerdfonts has been deprecated/removed in recent NixOS/nixpkgs versions.
+    # Individual fonts are now packaged under the `pkgs.nerd-fonts` attribute set.
+    # You can list specific ones you want to install:
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.symbols-only
   ];
+
+  # Note: If you want to install ALL nerd fonts (very large), uncomment the following line:
+  # fonts.packages = with pkgs; [ noto-fonts noto-fonts-cjk-sans noto-fonts-emoji fira-code ] ++ (builtins.filter pkgs.lib.isDerivation (builtins.attrValues pkgs.nerd-fonts));
 }
